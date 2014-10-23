ca07929db537f4cec02e22ae022ba445ad1fc07b
XXX <hello@mauroferrario.com>
2014-10-23 20:09:33 +0200

    Refactoring

--- src/ofApp.h
+++ src/ofApp.h
@@ -6,32 +6,26 @@
 #include "GoofyNodeButton.h"
 #include "GoofyNodeDelay.h"
 
-
-
-
 class ofApp : public ofBaseApp{
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+  void setup();
+  void update();
+  void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-  
+  void keyPressed(int key);
+  void keyReleased(int key);
+  void mouseMoved(int x, int y );
+  void mouseDragged(int x, int y, int button);
+  void mousePressed(int x, int y, int button);
+  void mouseReleased(int x, int y, int button);
+  void windowResized(int w, int h);
+  void dragEvent(ofDragInfo dragInfo);
+  void gotMessage(ofMessage msg);
+
   FakeClass* fake1;
   GoofyNodeStage* nodeView;
   GoofyNodeButton* button;
   GoofyNodeDelay* delay1;
   GoofyNodeDelay* delay2;
-
-  
-		
 };
